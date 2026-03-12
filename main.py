66cdc6a4b301dbf518dcefa4fc2b8bc086081b18
kuuuaiengineer <kuuu.ssc@gmail.com>
2026-03-12 14:50:33 +0000

    Add requirements.txt and Render対応

--- main.py
+++ main.py
@@ -62,4 +62,5 @@ def handle_message(event):
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_text))
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
